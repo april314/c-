08507ef194274a818a1ee7d41b2955382f3cfa61
Pavel N <tbigad@gmail.com>
2017-02-19 18:04:04 +0300

    update  task

--- strcop.cpp
+++ strcop.cpp
@@ -8,22 +8,32 @@ int str_copy(const char **strOut,const char *strIn);
 
 int main()
 {
-	char source[] = "Hello";
-	const char *destin[] = {"Bye"};		//array of pointers to const char: 
-										                //you can change the pointers themselves, 
+	char source[] = "Worst";
+	const char *destin = /*????*/;// = {"World"};		//array of pointers to const char: 
+										                //you can change the pointers themselves,
 										                //just not the characters pointed to by those pointers
 	cout << *destin << endl;
-	str_copy(destin, source);
+	if(0 != str_copy(destin, source)){
+
+	}
 	cout << *destin << endl;
-	
+
 	*destin = "hi";
 	cout << *destin << endl;
 	return 0;
 }
 
-int str_copy(const char **strOut, const char *strIn) 
+// W o r l d
+// o r 0 0 0
+// r d 0 0 0
+// s e 0 0 0
+// t r 0 0 0
+
+int str_copy(const char **strOut, const char *strIn)
 {
-	assert(strIn != NULL);
+	if(strIn != NULL){
+	    return -1;
+	}
 	int n = (strlen(strIn)+1);
 	char * stmp = (char*)malloc(n);
 	int i;
@@ -32,5 +42,8 @@ int str_copy(const char **strOut, const char *strIn)
 	}
 	stmp[i] = '\0';
 	*strOut = stmp;
+	// while(strmo != 0){
+	//   **strOut = *stmp;
+	// }
 	return 0;
 }
